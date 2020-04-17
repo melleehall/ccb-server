e7db17cffab7a66bb5dbc63cd1f483006126660a
Melanie Hallowell <melleehall@gmail.com>
2020-04-17 10:53:17 -0600

    Refactoring bookmark app to CCB form

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,4 @@
+INSERT INTO bookmarks (id, title, lastName, url, streetNum, streetName, city, zip, description, rating)
+VALUES
+  (1, 'Thinkful', 'Last name', 'https://www.thinkful.com', '123', 'Street name', 'A city', '12345', 'Think outside the classroom', '5'),
+  (2, 'Google', 'Hallowel', 'https://www.google.com', '123', 'Frog Street', 'Evergreen', '54637', 'Where we find everything else', '4')
