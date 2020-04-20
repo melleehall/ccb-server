07c245f11a0758a2b79fb6302b038e138dd2f7f9
Melanie Hallowell <melleehall@gmail.com>
2020-04-20 10:16:35 -0600

    Removed personal info from seed file and commented out currently unused routes as well as added xss to post method route

--- seeds/seed.bookmarks.sql
+++ seeds/seed.bookmarks.sql
@@ -1,4 +1,3 @@
 INSERT INTO contacts (id, firstname, lastname, email, phone, streetnum, streetname, city, zip)
 VALUES
-  (1, 'First', 'Last name', 'email@gmail.com', '303-303-3030', '111', 'City', '12345'),
-  (2, 'Mister', 'Moose', 'mistermoose@gmail.com', '313-313-3131', '123', 'Frog Street', 'Evergreen', '54637')
+  (1, 'First', 'Last name', 'email@gmail.com', '303-303-3030', '111', 'City', '12345')
--- src/contacts/contacts-router.js
+++ src/contacts/contacts-router.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const { isWebUri } = require('valid-url')
 const xss = require('xss')
 const logger = require('../logger')
 const ContactsService = require('./contacts-service')
@@ -9,25 +8,18 @@ const bodyParser = express.json()
 
 const serializeContact = contact => ({
   id: contact.id,
-  firstname: contact.firstname,
-  lastname: contact.lastname,
-  email: contact.email,
-  phone: contact.phone,
-  streetnum: contact.streetnum,
-  streetname: contact.streetname,
-  city: contact.city,
-  zip: contact.zip
+  firstname: xss(contact.firstname),
+  lastname: xss(contact.lastname),
+  email: xss(contact.email),
+  phone: xss(contact.phone),
+  streetnum: xss(contact.streetnum),
+  streetname: xss(contact.streetname),
+  city: xss(contact.city),
+  zip: xss(contact.zip)
 })
 
 contactsRouter
   .route('/contacts')
-  .get((req, res, next) => {
-    ContactsService.getAllContacts(req.app.get('db'))
-      .then(contacts => {
-        res.json(contacts.map(serializeContact))
-      })
-      .catch(next)
-  })
   .post(bodyParser, (req, res, next) => {
     for (const field of ['firstname', 'lastname', 'email', 'phone', 'streetnum', 'streetname', 'city', 'zip']) {
 
@@ -38,8 +30,6 @@ contactsRouter
     }
 
     const {firstname, lastname, email, phone, streetnum, streetname, city, zip, request_service, request_news } = req.body
-    console.log(request_service)
-    console.log(request_news)
 
     const newContact = { firstname, lastname, email, phone, streetnum, streetname, city, zip, request_service, request_news }
 
@@ -48,48 +38,55 @@ contactsRouter
       newContact
     )
       .then(contact => {
-        logger.info(`Card with id ${contact.id} created.`)
+        logger.info(`Contact with id ${contact.id} created.`)
         res
           .status(201)
           .location(`/contacts/${contact.id}`)
           .json(serializeContact(contact))
       })
       .catch(next)
-  })
+    })
+    // .get((req, res, next) => {
+    //   ContactsService.getAllContacts(req.app.get('db'))
+    //     .then(contacts => {
+    //       res.json(contacts.map(serializeContact))
+    //     })
+    //     .catch(next)
+    // })
 
-contactsRouter
-  .route('/contacts/:contact_id')
-  .all((req, res, next) => {
-    const { contact_id } = req.params
-    ContactsService.getById(req.app.get('db'), contact_id)
-      .then(contact => {
-        if (!contact) {
-          logger.error(`Contact with id ${contact_id} not found.`)
-          return res.status(404).json({
-            error: { message: `Contact Not Found` }
-          })
-        }
-        res.contact = contact
-        next()
-      })
-      .catch(next)
+// contactsRouter
+//   .route('/contacts/:contact_id')
+//   .all((req, res, next) => {
+//     const { contact_id } = req.params
+//     ContactsService.getById(req.app.get('db'), contact_id)
+//       .then(contact => {
+//         if (!contact) {
+//           logger.error(`Contact with id ${contact_id} not found.`)
+//           return res.status(404).json({
+//             error: { message: `Contact Not Found` }
+//           })
+//         }
+//         res.contact = contact
+//         next()
+//       })
+//       .catch(next)
 
-  })
-  .get((req, res) => {
-    res.json(serializeContact(res.contact))
-  })
-  .delete((req, res, next) => {
-    // TODO: update to use db
-    const { contact_id } = req.params
-    ContactsService.deleteContact(
-      req.app.get('db'),
-      contact_id
-    )
-      .then(numRowsAffected => {
-        logger.info(`Card with id ${contact_id} deleted.`)
-        res.status(204).end()
-      })
-      .catch(next)
-  })
+//   })
+//   .get((req, res) => {
+//     res.json(serializeContact(res.contact))
+//   })
+//   .delete((req, res, next) => {
+//     // TODO: update to use db
+//     const { contact_id } = req.params
+//     ContactsService.deleteContact(
+//       req.app.get('db'),
+//       contact_id
+//     )
+//       .then(numRowsAffected => {
+//         logger.info(`Contact with id ${contact_id} deleted.`)
+//         res.status(204).end()
+//       })
+//       .catch(next)
+//   })
 
 module.exports = contactsRouter
