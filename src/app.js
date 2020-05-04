18c590a81f8822d0b096c6a53d05f68b97af9e3e
Melanie Hallowell <melleehall@gmail.com>
2020-05-04 17:00:44 -0600

    Commented out cors CLIENT ORIGIN configuration in app.js until client is configued so forms can be submitted

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const {CLIENT_ORIGIN} = require('./config');
+// const {CLIENT_ORIGIN} = require('./config');
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 // const validateBearerToken = require('./validate-bearer-token')
@@ -13,11 +13,11 @@ const app = express()
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 app.use(morgan(morganSetting))
-app.use(
-  cors({
-      origin: CLIENT_ORIGIN
-  })
-);
+// app.use(
+//   cors({
+//       origin: CLIENT_ORIGIN
+//   })
+// );
 app.use(helmet())
 // app.use(validateBearerToken)
 
