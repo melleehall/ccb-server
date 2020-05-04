423e54aa4f32a979820c015574d75c690761356b
Melanie Hallowell <melleehall@gmail.com>
2020-05-04 17:18:36 -0600

    Enabling cors again

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express()
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 app.use(morgan(morganSetting))
-// app.use(cors())
+app.use(cors())
 // app.use(
 //   cors({
 //       origin: CLIENT_ORIGIN
@@ -27,10 +27,6 @@ app.use('/api/contacts', contactsRouter)
 // used to test connection for hosted server
 app.get('/hello', (req, res) => {
   res.send('Hello, world!')
-})
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
 app.use(errorHandler)
